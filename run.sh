a024b236f67a8948e1d7f8daf42f58e2446095d1
soapsnake <soapsnake@gmail.com>
2019-09-11 18:23:04 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-spark-submit --class "com.soapsnake.spark.app.SampleTest" --master local[4] /home/ubuntu/github/bigdata/target/scala-2.11/bigdata_2.11-0.1.jar
+spark-submit --class "com.soapsnaçke.spark.app.SampleTest" --master local[4] /home/ubuntu/github/bigdata/target/scala-2.11/bigdata_2.11-0.1.jar
--- src/main/scala/com/soapsnake/spark/stream/StreamSample.scala
+++ src/main/scala/com/soapsnake/spark/stream/StreamSample.scala
@@ -8,9 +8,10 @@ object StreamSample {
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
-    val ssc = new StreamingContext(conf, Seconds(1));
+    val ssc = new StreamingContext(conf, Seconds(5));
 
-    val lines = ssc.socketTextStream("localhost", 9999)
+      //lines 将会是一个DStreams
+    val lines = ssc.socketTextStream("localhost", 9999)  //Define the input sources by creating input DStreams.
 
     val words = lines.flatMap(_.split(" "))   //根据空格对输入的文子进行split,flatMap真的很精绥啊
 
